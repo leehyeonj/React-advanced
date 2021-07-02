95c1dc20c84810e78db5c086a9117b0f4d7fc19a
leehyeonj <billaa@naver.com>
2021-07-02 17:30:06 +0900

    로그인 페이지, Input, Button 컴포넌트

---
+++ image-community/src/elements/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = (props) =>{
+    const {text, _onClick}=props;
+    return(
+        <React.Fragment>
+            <ELButton onClick={_onClick}>{text}</ELButton>
+        </React.Fragment>
+    )
+}
+
+Button.defaultProps ={
+    text: "텍스트",
+    _onClick : () =>{}
+}
+
+const ELButton = styled.button`
+    width: 100%;
+    background-color : #212121;
+    color: #ffffff;
+    padding: 12px 0px;
+    box-sizing: border-box;
+    border: none;
+`;
+export default Button;
