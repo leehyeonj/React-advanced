499506940dadfb9de4d0e3c903c4d34928869533
leehyeonj <billaa@naver.com>
2021-07-02 17:37:03 +0900

    Header 만들기

--- image-community/src/shared/App.js
+++ image-community/src/shared/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
 import PostList from '../pages/PostList';
 import Login from '../pages/Login';
+import Header from '../components/Header';
+import {Grid} from "../elements";
 
 function App() {
   return (
     <React.Fragment>
+    <Grid>
+    <Header></Header>
       <BrowserRouter>
         <Route path="/" exact component={PostList}/>
         <Route path="/login" exact component={Login}/>
       </BrowserRouter>
+    </Grid>
     </React.Fragment>
   );
 }
