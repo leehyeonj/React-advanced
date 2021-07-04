5c56eea9a3cefb0b787d82ace34314c892cc5ca7
leehyeonj <billaa@naver.com>
2021-07-04 20:39:07 +0900

    상세페이지에 게시물 정보 뜨게 하기

--- image-community/src/pages/PostList.js
+++ image-community/src/pages/PostList.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useSelector , useDispatch} from "react-redux";
 
 import Post from "../components/Post";
+import { Grid } from "../elements";
 import { actionCreators as postActions } from "../redux/modules/post";
 import InfinityScroll from "../shared/InfinityScroll";
 
@@ -12,7 +13,7 @@ const PostList = (props) => {
     const user_info = useSelector((state)=> state.user.user);
     const is_loading = useSelector((state) => state.post.is_loading);
     const paging = useSelector((state) => state.post.paging);
-
+    const {history} = props;
     console.log(post_list);
     
     // 빈배열이 들어가면 처음에 한번만 componentDidMount
@@ -27,25 +28,48 @@ const PostList = (props) => {
     },[]);
 
     return (
-        <React.Fragment>
+      <React.Fragment>
+        <Grid bg={"#EFF6FF"} padding="20px 0px">
+          {/* <Post/> */}
           <InfinityScroll
-            callNext = {()=>{
+            callNext={() => {
               dispatch(postActions.getPostFB(paging.next));
             }}
-            is_next={paging.next? true:false}
-            loading= {is_loading}
+            is_next={paging.next ? true : false}
+            loading={is_loading}
           >
-            {/* p에는 post에 대한 모든 정보가 들어간다. */}
-          {post_list.map((p, idx)=>{
-              if(user_info && p.user_info.user_id === user_info.uid){
-                return <Post key={p.id} {...p} is_me/>
-              }else{
-                return <Post key={p.id} {...p} />
+            {post_list.map((p, idx) => {
+              if (p.user_info.user_id === user_info?.uid) {
+                return (
+                  <Grid
+                    bg="#ffffff"
+                    margin="8px 0px"
+                    key={p.id}
+                    _onClick={() => {
+                      history.push(`/post/${p.id}`);
+                    }}
+                  >
+                    <Post key={p.id} {...p} is_me />
+                  </Grid>
+                );
+              } else {
+                return (
+                  <Grid
+										key={p.id}
+                    bg="#ffffff"
+                    _onClick={() => {
+                      history.push(`/post/${p.id}`);
+                    }}
+                  >
+                    <Post {...p} />
+                  </Grid>
+                );
               }
-          })}
+            })}
           </InfinityScroll>
-        </React.Fragment>
-    )
+        </Grid>
+      </React.Fragment>
+    );
 }
 
 export default PostList;
