a7b8ee30ed62efdc6fb2c9942afa92469095d003
leehyeonj <billaa@naver.com>
2021-07-04 23:17:59 +0900

    내가 올린 게시물에 댓글이 달리면 알람이 온다.

--- image-community/src/components/NotiBadge.js
+++ image-community/src/components/NotiBadge.js
@@ -1,13 +1,29 @@
 import React from "react";
 import {Badge} from "@material-ui/core";
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { realtime } from "../shared/firebase";
+import { useSelector } from "react-redux";
 
 const NotiBadge = (props)=>{
     const[is_read, setIsRead] = React.useState(true);
 
+    const user_id = useSelector(state=> state.user.user.uid);
     const notiCheck = ()=>{
+        const notiDB = realtime.ref(`noti/${user_id}`);
+        notiDB.update({read: true});
         props._onClick();
-    }
+    };
+
+    React.useEffect(()=>{
+        const notiDB = realtime.ref(`noti/${user_id}`);
+        notiDB.on("value", (snapshot)=>{
+            console.log(snapshot.val());
+            setIsRead(snapshot.val().read);
+        });
+
+        return ()=> notiDB.off(); //구독해제
+    },[]);
+
     return(
         <React.Fragment>
              <Badge color="secondary" variant="dot" invisible={is_read} onClick={notiCheck}>
